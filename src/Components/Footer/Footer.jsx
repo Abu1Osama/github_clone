6575d22f5e5d08ba99a28514e8808f3deefc4734
Abu Osama <abuosamasports@gmail.com>
2023-11-22 14:22:35 +0530

    footer added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "../Footer/Footer.scss";
+
+function Footer() {
+  return (
+    <div id="Footer" className="Footer">
+      <div className="Footer-left">
+        <h1>Github</h1>
+        <strong>Subscribe to our developer newsletter</strong>
+        <p>Get tips, technical guides, and best practices. Once a month. Right in your inbox.</p>
+        <button>Subscribe</button>
+      </div>
+      <div className="Footer-right">
+        <div className="right-child">
+          <ul>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Enterprise</li>
+            <li>Copilot</li>
+            <li>Security</li>
+            <li>Pricing</li>
+            <li>Team</li>
+            <li>Resources</li>
+            <li>Roadmap</li>
+            <li>Compare GitHub</li>
+          </ul>
+        </div>
+        <div className="right-child">
+          <ul>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Enterprise</li>
+            <li>Copilot</li>
+            <li>Security</li>
+            <li>Pricing</li>
+            <li>Team</li>
+            <li>Resources</li>
+            <li>Roadmap</li>
+            <li>Compare GitHub</li>
+          </ul>
+        </div>
+        <div className="right-child">
+          <ul>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Enterprise</li>
+            <li>Copilot</li>
+            <li>Security</li>
+            <li>Pricing</li>
+            <li>Team</li>
+            <li>Resources</li>
+            <li>Roadmap</li>
+            <li>Compare GitHub</li>
+          </ul>
+        </div>
+        <div className="right-child">
+          <ul>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Enterprise</li>
+            <li>Copilot</li>
+            <li>Security</li>
+            <li>Pricing</li>
+            <li>Team</li>
+            <li>Resources</li>
+            <li>Roadmap</li>
+            <li>Compare GitHub</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
